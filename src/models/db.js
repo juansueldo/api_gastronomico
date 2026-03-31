9a373d528297b64083249ce7031c26273e5ed735
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2026-03-31 07:07:23 -0300

    Add pg module for PostgreSQL dialect support

--- src/models/db.js
+++ src/models/db.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 require('dns').setDefaultResultOrder('ipv4first');
 
 const { Sequelize } = require('sequelize');
-
+const pg = require('pg');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
